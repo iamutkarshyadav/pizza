ec0aacd93d9f62b2f293adc63b2e1f8fe9b4116a
Utkarsh Yadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-12-04 05:09:05 +0530

    initial

---
+++ server.js
@@ -0,0 +1,19 @@
+// create express server
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// set template engine
+
+app.use(expressLayouts);
+app.set("views", path.join(__dirname, "/resources/views")); // Fix the typo here
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}!`));
