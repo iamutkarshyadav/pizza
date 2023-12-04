c4a1de3ecf4734e6b9e27c2dc8ee94287dcabb88
Utkarsh Yadav <72760223+iamutkarshyadav@users.noreply.github.com>
2023-12-05 04:14:54 +0530

    database connected

--- server.js
+++ server.js
@@ -5,19 +5,29 @@ const app = express();
 const ejs = require("ejs");
 const expressLayouts = require("express-ejs-layouts");
 const path = require("path");
+const mongoose = require("mongoose");
 
-// setting up static files
+
+const url = "mongodb://0.0.0.0:27017/pizza"; // Change this line
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+
+connection
+  .once("open", () => {
+    console.log("Database connected...");
+  })
+  .on("error", (err) => {
+    console.log("Connection failed...", err);
+  });
 app.use(express.static("public"));
 
-// setting up template engine
+
 
 app.use(expressLayouts);
 app.set("views", path.join(__dirname, "/resources/views")); // Fix the typo here
 app.set("view engine", "ejs");
 
-// app.get("/", (req, res) => {
-//   res.render("home");
-// });
+
 
 require("./routes/web")(app);
 
